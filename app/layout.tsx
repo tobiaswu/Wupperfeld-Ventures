4088509b574115f8c561da5e271986d01faa5e98
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2024-11-22 11:45:39 -0300

    feat: update meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,6 @@ import { Footer } from '@/components/Footer';
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['300', '500'] });
 
-export const metadata: Metadata = {
-  title: 'Wupperfeld Ventures',
-  description:
-    'We at Wupperfeld Ventures are building and investing in technology-driven ventures. Turning innovative ideas into impactful solutions.',
-};
-
 export default function RootLayout({
   children,
 }: {
@@ -24,4 +18,29 @@ export default function RootLayout({
       </body>
     </html>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'Wupperfeld Ventures',
+    description:
+      'Building and investing in technology-driven ventures. Turning innovative ideas into impactful solutions.',
+    robots: { index: true, follow: true },
+    openGraph: {
+      title: 'Wupperfeld Ventures',
+      description:
+        'Building and investing in technology-driven ventures. Turning innovative ideas into impactful solutions.',
+      type: 'website',
+      images: [
+        {
+          url: '/wv_logo.webp',
+          width: 1730,
+          height: 744,
+          alt: 'Wupperfeld Ventures Logo',
+        },
+      ],
+      url: 'https://www.wupperfeld.org',
+      siteName: 'Wupperfeld Ventures',
+    },
+  };
 }
